0c5380cc10f71d3effe1a59d2cb5f1a65f6a7e56
peter <p@p34.eu>
2026-01-29 01:15:45 +0200

    Updated vite setup

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig, loadEnv } from "vite";
-
 export default defineConfig(({ mode }) => {
-  
   const env = loadEnv(mode, process.cwd(), "");
-
   return {
     build: {
-      assetsDir: "build", // put JS/CSS directly in dist/
+      assetsDir: "build", 
     },
     server: {
       proxy: {
